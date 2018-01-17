35e8e6e223aeadf7f902122504920c5b155f18e6
Ivan Avalos <ivan.avalos.diaz@hotmail.com>
2018-01-16 22:40:38 -0600

    Removed test files

--- MagicCarRemoteControl-backup/all.h
+++
@@ -1,7 +0,0 @@
-#ifndef ALL_H
-#define ALL_H
-
-#include "qtlibraries.h"
-#include "network.h"
-
-#endif // ALL_H
--- MagicCarRemoteControl-backup/qtlibraries.h
+++
@@ -1,8 +0,0 @@
-#ifndef QTLIBRARIES_H
-#define QTLIBRARIES_H
-
-#include <QMainWindow>
-#include <QInputDialog>
-#include <QMessageBox>
-
-#endif // QTLIBRARIES_H
